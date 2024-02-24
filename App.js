c53cd674273dc22d5ab75f8a19512925d7015659
shrey3105 <shreysingh3105@gmail.com>
2024-02-24 18:35:39 +0530

    Igniting React application and understanding the use of parcel bundler

--- App.js
+++ App.js
@@ -1,18 +1,5 @@
-/*
-
-<div id="parent">
-    <div id="child1">
-    <h1>I am h1 tag</h1>
-    <h2>I am h1 tag</h2>
-    </div>
-    <div id="child2">
-        <h1>I am h1 tag</h1>
-        <h2>I am h1 tag</h2>
-    </div>
-</div>
-
-
-*/
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
 const parent = React.createElement(
     "div",
